e0b7dc2f2e95cf699caf76228a149d613bae9798
BT_1N3_24@bktech.local <BT_1N3_24@bktech.local>
2018-11-14 10:17:56 -0500

    Initial commit

---
+++ src/TokenPass.java
@@ -0,0 +1,26 @@
+public class TokenPass {
+    private int[] board;
+            private int currentPlayer;
+
+    public TokenPass(int playerCount)
+    {
+        board = new int[playerCount];
+        for (int i = 0; i < playerCount; i++)
+        {
+            board[i] = 1 + (int) (playerCount * Math.random());
+        }
+    }
+    public void distributeCurrentPlayerTokens()
+    {
+        int nextPlayer = currentPlayer;
+        int numToDistribute = board[currentPlayer];
+        board[currentPlayer] = 0;
+
+        while (numToDistribute > 0)
+        {
+            nextPlayer = (nextPlayer + 1) % board.length;
+            board[nextPlayer]++;
+            numToDistribute--;
+        }
+    }
+}
